cebdee5aeffa562ed5e697c5f9443656b30b077c
Thomas Cashman <thomascashman404@gmail.com>
2018-02-22 00:00:14 +0100

    Improve MultiGetRequest Java API

--- api/src/main/java/com/elefana/api/document/MultiGetRequest.java
+++ api/src/main/java/com/elefana/api/document/MultiGetRequest.java
@@ -15,10 +15,14 @@
  ******************************************************************************/
 package com.elefana.api.document;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.elefana.api.ApiRequest;
 import com.elefana.api.RequestExecutor;
 
 public abstract class MultiGetRequest extends ApiRequest<MultiGetResponse> {
+	private final List<GetRequest> getRequests = new ArrayList<GetRequest>(1);
 	protected String indexPattern, typePattern, requestBody;
 	
 	public MultiGetRequest(RequestExecutor requestExecutor) {
@@ -28,6 +32,11 @@ public abstract class MultiGetRequest extends ApiRequest<MultiGetResponse> {
 	public MultiGetRequest(RequestExecutor requestExecutor, String requestBody) {
 		super(requestExecutor);
 		this.requestBody = requestBody;
+	}
+	
+	public void add(GetRequest getRequest) {
+		getRequests.add(getRequest);
+		this.requestBody = null;
 	}
 
 	public String getIndexPattern() {
@@ -53,7 +62,48 @@ public abstract class MultiGetRequest extends ApiRequest<MultiGetResponse> {
 	}
 
 	public String getRequestBody() {
-		return requestBody;
+		StringBuilder result = new StringBuilder();
+		if(requestBody != null) {
+			result.append(requestBody);
+		} else {
+			result.append('{');
+			result.append("\"docs\":[");
+			for(int i = 0; i < getRequests.size(); i++) {
+				GetRequest getRequest = getRequests.get(i);
+				result.append('{');
+				if(getRequest.getIndex() != null) {
+					result.append("\"_index\":\"");
+					result.append(getRequest.getIndex());
+					result.append('\"');
+				}
+				if(getRequest.getType() != null) {
+					if(getRequest.getIndex() != null) {
+						result.append(',');
+					}
+					
+					result.append("\"_type\":\"");
+					result.append(getRequest.getType());
+					result.append('\"');
+				}
+				if(getRequest.getId() != null) {
+					if(getRequest.getIndex() != null || getRequest.getType() != null) {
+						result.append(',');
+					}
+					
+					result.append("\"_id\":\"");
+					result.append(getRequest.getId());
+					result.append('\"');
+				}
+				result.append('}');
+				
+				if(i < getRequests.size() - 1) {
+					result.append(',');
+				}
+			}
+			result.append(']');
+			result.append('}');
+		}
+		return result.toString();
 	}
 
 	public void setRequestBody(String requestBody) {
