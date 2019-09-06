a2c97d6e87ebe1938122824bea1f756568b763e5
Raphael Gruber <raphael@viridiansoftware.com>
2019-09-06 18:44:18 +0100

    [#3] Basic Implementation of V2OsStats

--- core/src/main/java/com/elefana/node/v2/V2OsStats.java
+++ core/src/main/java/com/elefana/node/v2/V2OsStats.java
@@ -16,14 +16,61 @@
 package com.elefana.node.v2;
 
 import com.elefana.node.OsStats;
+import oshi.SystemInfo;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 public class V2OsStats extends OsStats {
 
 	@Override
 	protected void generateCurrentStats(Map<String, Object> result) {
-		
+		result.clear();
+		SystemInfo systemInfo = new SystemInfo();
+
+		double cpuLoad = systemInfo.getHardware().getProcessor().getSystemCpuLoad();
+		int roundedCpuLoad = (int)Math.round(cpuLoad * 100);
+		result.put("percent", roundedCpuLoad);
+
+		// First value in double array from getSystemLoadAverage is the average load for 1 minute
+		double cpuLoadAverage = systemInfo.getHardware().getProcessor().getSystemLoadAverage(1)[0];
+		int roundedCpuLoadAverage = (int)Math.round(cpuLoad * 100);
+		result.put("load_average", roundedCpuLoadAverage);
+
+		Map<String, Object> mem = new HashMap<>();
+
+		long totalMemory = systemInfo.getHardware().getMemory().getTotal();
+		mem.put("total_in_bytes", totalMemory);
+
+		long freeMemory = systemInfo.getHardware().getMemory().getAvailable();
+		mem.put("free_in_bytes", freeMemory);
+
+		long freePercent = (int)Math.round((freeMemory / totalMemory) * 100);
+		mem.put("free_percent", freePercent);
+
+		long usedMemory = totalMemory - freeMemory;
+		mem.put("used_in_bytes", usedMemory);
+
+		long usedPercent = (int)Math.round((usedMemory / totalMemory) * 100);
+		mem.put("used_percent", usedPercent);
+
+		result.put("mem", mem);
+
+		Map<String, Object> swap = new HashMap<>();
+
+		long totalSwap = systemInfo.getHardware().getMemory().getSwapTotal();
+		swap.put("total_in_bytes", usedPercent);
+
+		long usedSwap = systemInfo.getHardware().getMemory().getSwapUsed();
+		swap.put("used_in_bytes", usedPercent);
+
+		long freeSwap = totalSwap - usedSwap;
+		swap.put("free_in_bytes", usedPercent);
+
+		result.put("swap", swap);
+
+		long timestamp = new Date().getTime();
+		result.put("timestamp", timestamp);
 	}
-	
 }
