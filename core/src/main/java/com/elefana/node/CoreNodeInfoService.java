57f8b63cc886e5fad5999f72a54f91bdc76a7788
Raphael Gruber <raphael.gruber@gmail.com>
2019-09-10 16:11:04 +0100

    [#8] Make node stats refresh interval configurable (#12)
    * [#8] Make node stats refresh interval configurable
    
    elefana.service.node.statsRefreshInterval specifies how many seconds
    elapse between fetching new node statistics
    
    * [#8] Document new property in *.properties
    
    * [#8] Fix copyright header in CoreNodeInfoService

--- core/src/main/java/com/elefana/node/CoreNodeInfoService.java
+++ core/src/main/java/com/elefana/node/CoreNodeInfoService.java
@@ -1,6 +1,18 @@
-/**
- * Copyright 2018 Viridian Software Ltd.
- */
+/*******************************************************************************
+ * Copyright 2019 Viridian Software Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package com.elefana.node;
 
 import com.elefana.api.json.EmptyJsonObject;
@@ -82,9 +94,11 @@ public class CoreNodeInfoService implements NodeInfoService {
 			break;
 		}
 
-		scheduledExecutorService.scheduleAtFixedRate(jvmStats, 0L, 1L, TimeUnit.SECONDS);
-		scheduledExecutorService.scheduleAtFixedRate(osStats, 0L, 1L, TimeUnit.SECONDS);
-		scheduledExecutorService.scheduleAtFixedRate(processStats, 0L, 1L, TimeUnit.SECONDS);
+		long refreshInterval = environment.getProperty("elefana.service.node.statsRefreshInterval", Long.class, 1L);
+
+		scheduledExecutorService.scheduleAtFixedRate(jvmStats, 0L, refreshInterval, TimeUnit.SECONDS);
+		scheduledExecutorService.scheduleAtFixedRate(osStats, 0L, refreshInterval, TimeUnit.SECONDS);
+		scheduledExecutorService.scheduleAtFixedRate(processStats, 0L, refreshInterval, TimeUnit.SECONDS);
 	}
 
 	@PreDestroy
