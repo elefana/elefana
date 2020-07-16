23a450ae2a1ee91d8ebb5c0247704828688bc16e
Thomas Cashman <thomascashman404@gmail.com>
2020-07-16 22:39:02 +0100

    Create ThreadLocalByteArray.java

---
+++ api/src/main/java/com/elefana/api/util/ThreadLocalByteArray.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright 2020 Viridian Software Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.elefana.api.util;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ThreadLocalByteArray extends ThreadLocal<byte[]> {
+	private static final AtomicInteger MAX_SIZE = new AtomicInteger(2048);
+
+	@Override
+	protected byte[] initialValue() {
+		return new byte[MAX_SIZE.get()];
+	}
+
+	@Override
+	public void set(byte[] value) {
+		MAX_SIZE.set(Math.max(MAX_SIZE.get(), value.length));
+		super.set(value);
+	}
+}
