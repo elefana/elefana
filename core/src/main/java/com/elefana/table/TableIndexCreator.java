2320e016055883b32c0e7fd35c8a11dfc7087a43
Thomas Cashman <thomascashman404@gmail.com>
2020-10-19 14:41:35 +0100

    Fix field GIN index creation

--- core/src/main/java/com/elefana/table/TableIndexCreator.java
+++ core/src/main/java/com/elefana/table/TableIndexCreator.java
@@ -198,7 +198,7 @@ public class TableIndexCreator implements Runnable {
 		}
 		if(ginEnabled) {
 			final String ginIndexName = getPsqlIndexName(IndexUtils.GIN_INDEX_PREFIX, tableName, fieldName);
-			final String query = "CREATE INDEX IF NOT EXISTS " + ginIndexName + " ON " + tableName + " USING GIN ((_source->>'" + fieldName + "'));";
+			final String query = "CREATE INDEX IF NOT EXISTS " + ginIndexName + " ON " + tableName + " USING GIN ((_source->'" + fieldName + "'));";
 			LOGGER.info(query);
 			PreparedStatement preparedStatement = connection.prepareStatement(query);
 			try {
